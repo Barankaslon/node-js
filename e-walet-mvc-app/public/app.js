6cb1d009cbb465cb2e3247637aa0903c9caedd2e
Alex <barankaslon@hotmail.com>
2021-05-01 19:03:34 -0300

    added calendar

--- e-walet-mvc-app/public/app.js
+++ e-walet-mvc-app/public/app.js
@@ -4,3 +4,12 @@ document.querySelectorAll('.number__format').forEach(node => {
         currency: 'CAD' 
         }).format(node.textContent)
     })
+
+const dayNumber = new Date().getDate()
+const year = new Date().getFullYear()
+const dayName = new Date().toLocaleString("default", {weekday: "long"})
+const monthName = new Date().toLocaleString("default", {month: "long"})
+
+document.querySelector(".month__name").innerHTML = monthName
+document.querySelector(".date__number").innerHTML = dayNumber
+document.querySelector(".year").innerHTML = year
