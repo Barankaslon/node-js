e1bb76b5e11c292f080735f6edb380701878c96d
Alex <barankaslon@hotmail.com>
2021-05-02 19:34:08 -0300

    added wage-edit page

--- e-walet-mvc-app/routes/income.js
+++ e-walet-mvc-app/routes/income.js
@@ -12,6 +12,19 @@ router.get('/', async (req, res) => {
     })
 })
 
+router.get('/:id/edit', async (req, res) => {
+    if(!req.query.allow) {
+        return res.redirect('/')
+    }
+
+    const wage = await Income.getById(req.params.id)
+
+    res.render('wage-edit', {
+        title: `Edit`,
+        wage
+    })
+})
+
 router.get('/:id', async (req, res) => {
     const wages = await Income.getById(req.params.id)
     res.render('wage', {
