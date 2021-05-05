f1943e4284e8f7e02d8374decfa3c6bb0e8ea9ee
Alex <barankaslon@hotmail.com>
2021-05-05 19:13:04 -0300

    create user model

---
+++ e-walet-mvc-app/models/user.js
@@ -0,0 +1,39 @@
+const {Schema, model} = require('mongoose')
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    sum: {
+        total: [
+            {
+                incomes: {
+                    type: Number,
+                    required: true,
+                    default: 0
+                },
+                savings: {
+                    type: Number,
+                    required: true,
+                    default: 0
+                },
+                investments: {
+                    type: Number,
+                    required: true,
+                    default: 0
+                },
+                incomeId: {
+                    type: Schema.Types.ObjectId,
+                    required: true
+                }
+            }
+        ]
+    }
+})
+
+module.exports = model('User', userSchema)
