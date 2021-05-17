3acedebb8c0d9fa1eb8905ce56c095203981104c
Alexey Gvozdev <72240384+Barankaslon@users.noreply.github.com>
2021-05-17 11:19:00 -0300

    Delete e-walet-mvc-app/keys directory

--- e-walet-mvc-app/keys/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    MONGODB_URI: 'mongodb+srv://admin:4I4ZvtUP8CSWvaSX@cluster0.j7czu.mongodb.net/income', 
-    SESSION_SECRET: 'some secret value',
-    SENDGRID_API_KEY: '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'
-}
